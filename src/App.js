7b4a557f60597b15dbded3437c11faaacedb6b17
aviraj0403 <aviraj0403@gmail.com>
2025-04-16 17:30:26 +0530

    add cors

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ import {logSessionActivity} from './middlewares/logSessionActivity.js';
 import cors from 'cors';
 const app = express();  
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://grocery-ui-one.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',  
-  credentials: true, 
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
 
 app.use(express.json());  
 app.use(cookieParser());
