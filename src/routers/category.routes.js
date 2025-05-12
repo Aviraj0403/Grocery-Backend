cffe818619343469e35dee24365fc50102e502d1
aviraj0403 <aviraj0403@gmail.com>
2025-05-12 21:15:17 +0530

    update

--- src/routers/category.routes.js
+++ src/routers/category.routes.js
@@ -4,7 +4,8 @@ import {
   getAllCategories,
   getCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
+  getMainCategories
 } from '../controllers/category.controller.js';
 import  upload  from '../middlewares/upload.js';
 import {uploadImageToCloudinary} from '../controllers/imageUploadController.js'
@@ -14,6 +15,7 @@ const router = express.Router();
 
 router.post("/createCategory", upload.single('image'), verifyToken, uploadImageToCloudinary, createCategory);
 router.get("/getAllCategories", getAllCategories);
+router.get("/getMainCategories", getMainCategories);
 router.get("/getCategory/:id", getCategory);
 router.put("/updateCategory/:id", upload.single('image'),verifyToken, uploadImageToCloudinary,updateCategory);
 router.delete("/deleteCategory/:id", deleteCategory);
