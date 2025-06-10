58840dfaa8125fbd994f69cea9fe5e526814b80f
aviraj0403 <aviraj0403@gmail.com>
2025-06-10 22:20:12 +0530

    add url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://grocery-ui-one.vercel.app'
+  'https://grocery-ui-one.vercel.app',
+  'https://www.shanumart.in/'
 ];
 
 app.use(cors({
